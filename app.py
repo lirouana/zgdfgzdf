8f13f1b9874715dca3cb76325748fb8bb2d55cf6
lirouana <ana.misquita@aluno.faculdadeimpacta.com.br>
2025-04-02 16:26:30 -0300

    tirando comentários

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-# Exemplo de código com operações matemáticas
 num1 = 10
 num2 = 5
-
-# Soma
 soma = num1 + num2
 print(f'A soma de {num1} e {num2} é {soma}')
-
-# Multiplicação
 multiplicacao = num1 * num2
 print(f'A multiplicação de {num1} e {num2} é {multiplicacao}')
